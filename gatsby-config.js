6acb97f881af84e1e49c070f86598c8757b5a62b
Javier Escacena Sánchez <fjescacena@paradigmadigital.com>
2021-11-10 14:29:16 +0100

    sass added

--- gatsby-config.js
+++ gatsby-config.js
@@ -8,6 +8,7 @@ module.exports = {
     "gatsby-plugin-image",
     "gatsby-plugin-react-helmet",
     "gatsby-plugin-sitemap",
+    "gatsby-plugin-sass",
     {
       resolve: "gatsby-plugin-manifest",
       options: {
--- src/components/layout.js
+++ src/components/layout.js
@@ -1,5 +1,6 @@
 import * as React from "react";
 import { Link, useStaticQuery, graphql } from "gatsby";
+import "../styles/main.scss";
 import {
   container,
   heading,
