8255315c0f1dd9ba4d9cc27b807e41d0ea934084
Javier Escacena Sánchez <fjescacena@paradigmadigital.com>
2021-11-16 10:21:57 +0100

    fix meta viewport 2

--- src/components/javiescacena-home-layout.js
+++ src/components/javiescacena-home-layout.js
@@ -1,13 +1,12 @@
 import * as React from "react";
 import { Link, useStaticQuery, graphql } from "gatsby";
 import "./javiescacena-home-layout.scss";
-import { useMediaQuery } from 'react-responsive';
-
+import { useMediaQuery } from "react-responsive";
 
 const JaviEscacenaHomeLayout = ({ children }) => {
-   const isMobile = useMediaQuery({
-    query: '(max-width: 1024px)'
-  })
+  const isMobile = useMediaQuery({
+    query: "(max-width: 1024px)",
+  });
   const data = useStaticQuery(graphql`
     query {
       site {
@@ -17,14 +16,12 @@ const JaviEscacenaHomeLayout = ({ children }) => {
       }
     }
   `);
-  return (
+  return isMobile !== null ? (
     <div className="container">
-      <p>{isMobile ? 'mobile' : 'desktop'}</p>
-      <main className={isMobile ? 'mobile' : 'desktop'}>
-        {children}
-      </main>
+      <p>{isMobile ? "mobile" : "desktop"}</p>
+      <main className={isMobile ? "mobile" : "desktop"}>{children}</main>
     </div>
-  );
+  ) : null;
 };
 
 export default JaviEscacenaHomeLayout;
